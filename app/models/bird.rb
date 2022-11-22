9c45f5b15d5ea54c1682199777496f83ad31e244
Justin Jeffries <justincyjeffries@gmail.com>
2022-11-22 01:59:49 -0500

    commit tree 5269339d3c98c6c4c2ffe9371b78db9b7da97d17

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,13 @@
 class Bird < ApplicationRecord
+    def show
+        # params[:id] refers to the dynamic part of our route, defined by :id
+        # a request to /birds/2 would give params[:id] a value of 2
+        bird = Bird.find_by(id: params[:id])
+        render json: bird
+      end
+      def create
+        bird = Bird.create(name: params[:name], species: params[:species])
+        render json: bird, status: :created
+      end
+        
 end
